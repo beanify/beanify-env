690a8bc3bec7afb05236656ccbc3a475e5b0b499
445212619@qqcom <beanjs>
2020-12-25 15:27:37 +0800

    1.update 3.0.1

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const envSchema = require('env-schema')
 
-module.exports = function (beanify, opts, done) {
-  try {
-    const config = envSchema(opts)
-    beanify.decorate('$env', config)
-    done()
-  } catch (e) {
-    done(e)
-  }
+module.exports = async function (beanify, opts) {
+  const config = envSchema(opts)
+  beanify.decorate('$env', config)
 }
